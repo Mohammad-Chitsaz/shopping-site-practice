4e09ede04255aecaac671e9bdca099e33f8ca25a
Mohammad Mc <mohammad.chitsaz80@gmail.com>
2022-11-28 16:30:32 +0330

    add product to cart and save cart to localStorage

--- js/index.js
+++ js/index.js
@@ -7,6 +7,7 @@ const cartModal = document.querySelector('.cart');
 const cartBackBtn = document.querySelector('.back');
 const cartConfirmBtn = document.querySelector('.cart-confirm');
 const productsDOM = document.querySelector('.products-center');
+let cart = [];
 
 class Products {
   getProducts() {
@@ -17,24 +18,23 @@ class Products {
 class UI {
   displayProducts(products) {
     let result = '';
-
     products.forEach(product => {
       const productPrice = new Intl.NumberFormat('fa-IR').format(product.price);
       result += `
-        <div class="product">
-          <div class="img-container">
-            <img
-              class="product__img"
-              src="${product.imageUrl}"
-              alt="${product.alt}"
-            />
-          </div>
-          <div class="product__desc">
-            <p class="product__title">${product.title}</p>
-            <p class="product__price">${productPrice}</p>
-          </div>
-          <button class="add-to-cart" data-id="${product.id}">اضافه به سبد خرید</button>
+      <div class="product">
+        <div class="img-container">
+          <img
+            class="product__img"
+            src="${product.imageUrl}"
+            alt="${product.alt}"
+          />
         </div>
+        <div class="product__desc">
+          <p class="product__title">${product.title}</p>
+          <p class="product__price">${productPrice}</p>
+        </div>
+        <button class="add-to-cart" data-id="${product.id}">اضافه به سبد خرید</button>
+      </div>
       `;
       productsDOM.innerHTML = result;
     });
@@ -42,13 +42,39 @@ class UI {
 
   getAddToCartButtons() {
     const buttons = [...document.querySelectorAll('.add-to-cart')];
-    buttons.forEach(button => {});
+
+    buttons.forEach(button => {
+      const id = button.dataset.id;
+
+      const isInCart = cart.find(product => product.id === id);
+
+      if (isInCart) {
+        e.target.innerText = 'اضافه شد';
+        e.target.disabled = true;
+      }
+
+      button.addEventListener('click', e => {
+        e.target.innerText = 'اضافه شد';
+        e.target.disabled = true;
+
+        // get product from products
+        const addedProduct = { ...Storage.getProduct(id), quantity: 1 };
+        // add product to cart
+        cart = [...cart, addedProduct];
+        // save cart to local storage
+      });
+    });
   }
 }
 
 class Storage {
   static saveProducts(products) {
     localStorage.setItem('products', JSON.stringify(products));
+  }
+
+  static getProduct(id) {
+    const products = JSON.parse(localStorage.getItem('products'));
+    return products.find(product => product.id === parseInt(id));
   }
 }
 
